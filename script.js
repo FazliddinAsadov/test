5dee90c80f66ee9887a32923bc43e4f49e57c67e
asadovfazliddin11@gmail.com <asadovfazliddin11@gmail.com>
2022-04-20 10:19:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use stirct";
+
+// const a = confirm("siz 23 yoshdamisz");
+
+// if(a == true){
+//     alert("sizga mn");
+// }else{
+//     alert("munkin emas");
+// }
+
+const root = document.getElementById("root");
+
+let a = prompt("ismingizni kiriting");
+let b = prompt("yoshingizni kiriting");
+let c = prompt("addresingizni kiriting");
+
+const arr = [];
+arr.push(a);
+
+
+root.innerHTML = arr;
